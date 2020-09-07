b03e7467fd7697f51af528742de2f78d0b233186
Fernando Carrasco <fernando.carrasco0@gmail.com>
2020-09-07 19:07:53 -0300

    fixing indentation

--- trabalhos/tarefa_pratica_1/t1/src/basic.renderer.js
+++ trabalhos/tarefa_pratica_1/t1/src/basic.renderer.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
-typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-typeof define === 'function' && define.amd ? define(['exports'], factory) :
-(global = global || self, factory(global.BasicRenderer = {}));
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+  typeof define === 'function' && define.amd ? define(['exports'], factory) :
+  (global = global || self, factory(global.BasicRenderer = {}));
 }(this, (function (exports) { 'use strict';
   
   const rastPolygon = () => {
